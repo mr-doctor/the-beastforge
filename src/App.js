09ebab88e0bb012682b0a2dd1fe344dd5e67a668
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2019-04-22 01:17:46 +1200

    Add damage and condition modifiers

--- src/App.js
+++ src/App.js
@@ -63,6 +63,12 @@ class App extends Component {
 				bonus: 0,
 				stat: "",
 			},
+			damageMods: {
+				damageMods: [],
+				damageType: "",
+				modifier: "",
+			},
+			conditionImmunities: [],
 		};
 	}
 
@@ -490,6 +496,75 @@ class App extends Component {
 							</Col>
 							<Col>
 								<Button onClick={this.addSkill}>Add</Button>
+							</Col>
+						</Form.Row>
+					</Form.Group>
+				</Form>
+
+				<Form>
+					<Form.Group>
+						<Form.Row>
+							<Col><br></br></Col>
+							<Col>
+								<h5>Condition Immunities</h5>
+							</Col>
+							<Col><br></br></Col>
+							<Col><br></br></Col>
+							<Col>
+								<h5>Damage Modifiers</h5>
+							</Col>
+							<Col><br></br></Col>
+						</Form.Row>
+						<Form.Row>
+							<Col>
+								<Form.Control id="condition-select" as="select" onChange={this.changeConditionImmunity}>
+									<option>Blinded</option>
+									<option>Charmed</option>
+									<option>Deafened</option>
+									<option>Encumbered</option>
+									<option>Exhaustion</option>
+									<option>Frightened</option>
+									<option>Grappled</option>
+									<option>Incorporeal</option>
+									<option>Intoxicated</option>
+									<option>Invisible</option>
+									<option>Paralysed</option>
+									<option>Petrified</option>
+									<option>Petrified</option>
+									<option>Poisoned</option>
+									<option>Restrained</option>
+									<option>Stunned</option>
+									<option>Unconscious</option>
+								</Form.Control>
+							</Col>
+							<Col>
+								<Button onClick={this.addConditionImmunity}>Add</Button>
+							</Col>
+							<Col>
+								<Form.Control id="damage-select" as="select" onChange={this.changeDamageType}>
+									<option>Bludgeoning</option>
+									<option>Fire</option>
+									<option>Force</option>
+									<option>Lightning</option>
+									<option>Necrotic</option>
+									<option>Piercing</option>
+									<option>Poison</option>
+									<option>Psychic</option>
+									<option>Radiant</option>
+									<option>Slashing</option>
+									<option>Thunder</option>
+									<option>Physical nonmagical</option>
+								</Form.Control>
+							</Col>
+							<Col>
+								<Form.Control id="modifier-select" as="select" onChange={this.changeDamageModifier}>
+									<option>Resistance</option>
+									<option>Immunity</option>
+									<option>Vulnerability</option>
+								</Form.Control>
+							</Col>
+							<Col>
+								<Button onClick={this.addDamageModifier}>Add</Button>
 							</Col>
 						</Form.Row>
 					</Form.Group>
