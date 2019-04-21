7c48037597a8b95d09dc6e1e459891e287b44d52
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2019-04-21 22:15:57 +1200

    Add skill addition

--- src/App.js
+++ src/App.js
@@ -56,6 +56,13 @@ class App extends Component {
 			languages: [],
 			language: "",
 			customLanguage: "",
+			skills: {
+				skillList: [],
+				skill: "",
+				proficient: false,
+				bonus: 0,
+				stat: "",
+			},
 		};
 	}
 
@@ -435,6 +442,58 @@ class App extends Component {
 					</Form.Group>
 				</Form>
 
+				<h2>Skills</h2>
+
+				<Form>
+					<Form.Group>
+						<Form.Row>
+							<Col>
+								<Form.Label>Skill</Form.Label>
+							</Col>
+							<Col>
+								<br></br>
+							</Col>
+							<Col>
+								<Form.Label>Bonus</Form.Label>
+							</Col>
+							<Col>
+								<br></br>
+							</Col>
+						</Form.Row>
+						<Form.Row>
+							<Col>
+								<Form.Control id="skill-select" as="select" onChange={this.changeSkill}>
+									<option>Athletics (Str)</option>
+									<option>Acrobatics (Dex)</option>
+									<option>Sleight of Hand (Dex)</option>
+									<option>Stealth (Dex)</option>
+									<option>Arcana (Int)</option>
+									<option>History (Int)</option>
+									<option>Investigation (Int)</option>
+									<option>Religion (Int)</option>
+									<option>Animal Handling (Wis)</option>
+									<option>Insight (Wis)</option>
+									<option>Medicine (Wis)</option>
+									<option>Perception (Wis)</option>
+									<option>Survival (Wis)</option>
+									<option>Deception (Cha)</option>
+									<option>Intimidation (Cha)</option>
+									<option>Performance (Cha)</option>
+									<option>Persuasion (Cha)</option>
+								</Form.Control>
+							</Col>
+							<Col>
+								<Form.Check type="checkbox" label="Proficient" id="proficient" onChange={this.changeSkill}/>
+							</Col>
+							<Col>
+								<Form.Control id="skill-bonus" type="number" step={1} value={this.state.skills.bonus} onChange={this.changeSkill} />
+							</Col>
+							<Col>
+								<Button onClick={this.addSkill}>Add</Button>
+							</Col>
+						</Form.Row>
+					</Form.Group>
+				</Form>
 
 				<Button onClick={this.debugButton}>Debug</Button>
 			</div>
@@ -443,6 +502,40 @@ class App extends Component {
 
 	debugButton = () => {
 		console.log(this.state);
+	}
+
+	addSkill = () => {
+		let skillTemp = {...this.state.skills};
+
+		skillTemp.skillList.push({
+			stat: skillTemp.stat,
+			proficient: skillTemp.proficient,
+			skill: skillTemp.skill,
+			bonus: skillTemp.bonus
+		});
+
+		this.setState({ skills: skillTemp });
+	}
+
+	changeSkill = () => {
+		let skillTemp = {...this.state.skills}
+		
+		let skillStrSplit = document.getElementById("skill-select").value.split(" ");
+
+		let stat = skillStrSplit[1].replace(/[()]/g, '').toLowerCase();
+
+		let isProf = !skillTemp.proficient;
+
+		let skillName = skillStrSplit[0];
+
+		let bonus = document.getElementById("skill-bonus").value;
+
+		skillTemp.stat = stat;
+		skillTemp.proficient = isProf;
+		skillTemp.skill = skillName;
+		skillTemp.bonus = bonus;
+
+		this.setState({ skills: skillTemp });
 	}
 
 	changeLanguage = (e) => {
