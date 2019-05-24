368f1286a051b87cf1a1be46865dbd9f17128e8c
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2019-05-24 20:48:17 +1200

    Add spell listing with styles

--- src/display/SpellList.js
+++ src/display/SpellList.js
@@ -11,17 +11,19 @@ class SpellList extends Component {
 			<ListGroup className="list-group" style={{ height: "150px", maxHeight: "150px" }}>
 				{this.props.list.map((spell) => {
 					return (
-						<ListGroup.Item key={spell.name + spell.level} id={spell.name + " " + spell.level}>
-							{spell.name + " | " + spell.level}
-							<Button onClick={() => {this.props.delete(spell.name, spell.level);}}>
-								{/* <MDBIcon icon="trash"></MDBIcon> */}
-							</Button>
+						<ListGroup.Item key={spell.name + spell.level} id={spell.name + " " + spell.level} className="spell-display-parent">
+							<div className="spell-display-align-centre">{spell.name + " | " + spell.level}</div>
+							<div className="delete-button-align-right">
+								<Button variant="danger" onClick={() => {this.props.delete(spell.name, spell.level);}}>
+									Delete
+								</Button>
+							</div>
 						</ListGroup.Item>
 					)
 				})
 				}
 			</ListGroup>
-		);
+		); 
 	}
 }
 
