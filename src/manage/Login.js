1dc17618b2161079c991766c7e0666a027ca5141
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2019-05-29 00:54:22 +1200

    Fix warnings

--- src/manage/Login.js
+++ src/manage/Login.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-import Overlay from 'react-bootstrap/Overlay';
-import Tooltip from 'react-bootstrap/Tooltip';
 
 class Saver extends Component {
 	api = "https://jhxwb4ferb.execute-api.us-west-2.amazonaws.com/prod";
 	// api = "http://127.0.0.1:5000"
-
-	constructor(...args) {
-		super(...args);
-	
-	}
 
 	render() {
 		return (
